191fde93eeb549336e53d31600a81fcd91d39302
EC2 Default User <yotchan32323@gmail.com>
2023-01-10 15:43:48 +0000

    Javaの基本

---
+++ Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "yotchan";
+		System.out.println(str);
+
+		str = "よっちゃん";
+		System.out.println(str);
+
+		final int LUCKY_NUMBER = 117;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
