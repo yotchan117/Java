dff5cd1183a174faf3816126e79444f9e5c2c0c8
EC2 Default User <yotchan32323@gmail.com>
2023-01-10 16:25:36 +0000

    chapter5-lesson

---
+++ Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		double d = 1234567.89;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 0.123f;
+		System.out.println(f);
+		Integer i = 5;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+	}
+}
